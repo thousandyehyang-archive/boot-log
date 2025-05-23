beff903025c1ac773c0ab9546571b25926bc0011
1000hyehyang <ducogus12@gmail.com>
2025-05-23 10:27:23 +0900

    feat: first commit

---
+++ src/test/java/org/example/bootlog/BootLogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
